e81f53afd39b5c1e6ffd64cac8b24fb59f4f7aef
Aditya Kamat <GiVeMeRoOt@users.noreply.github.com>
2018-02-12 23:05:04 +0530

    Packages installation

---
+++ LFS-Class3/setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get install xorriso
+sudo apt-get install grub
+sudo apt-get install gcc
+sudo apt-get install nasm
+sudo apt-get install virtualbox
+sudo apt-get install qemu
+sudo apt-get install gedit
